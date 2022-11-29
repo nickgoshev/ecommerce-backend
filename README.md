35fa0e635417e0eafdb76b6b836000bcc94d011e
nickgoshev <107017215+nickgoshev@users.noreply.github.com>
2022-11-29 08:28:03 -0700

    full upload
    did not have multiple smaller commits as i did most of the work on a clone of the starter code repository so i was unable to commit while making progress.

---
+++ README.md
@@ -0,0 +1,53 @@
+
+# ecommerce
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+a sequelize and  mysql db using node/express that allows the user to create/update/delete/view products, categories, and tags. I have attached a walkthrough using insomnia that goes through the process from setting up the db, seeding, and running the server.
+
+walkthrough: 
+https://drive.google.com/file/d/1qS23EQvpNH4tBuESj46Z8vzA3mNrFOov/view?usp=sharing
+
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+Run the following command to install necessary dependencies:
+
+```
+npm install
+```
+
+## Usage
+``` 
+mysql -p
+SOURCE db/schema.sql
+exit
+```
+```npm run seed```
+```npm start```
+
+
+
+## License
+
+This project is licensed under MIT.
+
+## Questions
+
+If you have questions, open an issue or contact me directly at nickgosh420@gmail.com. 
+You can find more of my work at [nickgoshev](https://github.com/nickgoshev/)
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+
+  Tag.findAll({
+    include:[
+      {model:Product, through:ProductTag }
+    ]
+  })
+  .then((tags)=>{
+    res.status(200).json(tags)
+  }).catch((err) => {
+    // console.log(err);
+    res.status(500).json(err);
+  });
+
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+
+  Tag.findByPk(req.params.id,{
+    include:[
+      {model:Product, through:ProductTag }
+    ]
+  })
+  .then((tags)=>{
+    res.status(200).json(tags)
+  }).catch((err) => {
+    // console.log(err);
+    res.status(500).json(err);
+  });
+
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+
+  Tag.create(req.body).then((newTag)=>{
+    res.status(200).json(newTag);
+  }).catch((err)=>{
+    res.status(400).json(err);
+  })
+
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+
+  Tag.update(
+    {tag_name:req.body.tag_name},
+    {where: {id:req.params.id}},
+  ).then((updatedTag)=>{
+    res.status(200).json(updatedTag);
+  }).catch((err)=>{
+    res.status(500).json(err);
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  
+  Tag.destroy({
+    where:{
+      id:req.params.id
+    }
+  }).then((deletedTag)=> {
+    res.status(200).json(deletedTag)
+  }).catch((err)=>
+  {
+    res.status(500).json(err);
+  })
+});
+
+module.exports = router;
